637669da7cb5770f790ccc2b7fcd451473674079
David Padilla <dpadag123@iesmarquesdecomares.org>
2021-02-17 18:45:18 +0100

    Tres en Raya Release

--- src/juego/CuadroDeJuego.java
+++ src/juego/CuadroDeJuego.java
@@ -1,28 +1,181 @@
 package juego;
 
-public class CuadroDeJuego {
-	
-	protected int x, y; // Coordenadas x e y del cuadro
-	protected int ancho = 600 / 3, alto = 600 / 3; // ancho y alto que ocupa el cuadro en pantalla
-	protected int xSuperior, ySuperior;
+import java.awt.Color;
+import java.awt.Graphics;
 
-	public CuadroDeJuego() {
-		
+import javax.swing.JOptionPane;
+
+public class CuadroDeJuego {
+	//Para facilitar la instancia del Tablero, crear aquí una referencia
+	private Tablero tablero = (Tablero) Tablero.getInstance();
+	protected int x, y; // Coordenadas x e y del cuadro
+	protected int ancho, alto; // ancho y alto que ocupa el cuadro en pantalla
+	protected int xSuperior, ySuperior; //La x y la y de la parte superior de cada cuadro, servirá para poder pintar cada cuadro
+	private int turnoJugador = 0;
+
+	/**
+	 * Constructor por defecto
+	 */
+	public CuadroDeJuego() {	
 	}
 
+	/**
+	 * Constructor con todos los parámetros con el que se creará cada cuadro 
+	 * @param x
+	 * @param y
+	 */
 	public CuadroDeJuego(int x, int y) {
 		super();
 		this.x = x;
 		this.y = y;
-		xSuperior = x * Tablero.getInstance().getCanvas().getWidth();
-		ySuperior = y * Tablero.getInstance().getCanvas().getHeight();
-	}
-
-	public void click(int x, int y, int ancho, int alto) {
-		this.x = x;
-		this.y = y;
+		//Calcular el tamaño de un cuadro dividiendo tanto anchura como altura entre tres
+		this.ancho = Tablero.getInstance().getWidth()/3;  
+		this.alto = Tablero.getInstance().getHeight()/3;
+		xSuperior = this.x * this.ancho;
+		ySuperior = this.y * this.alto;
+		
 	}
 	
+	/**
+	 * Método paint con el que se pintará cada cuadro
+	 * @param g
+	 */
+	public void paint(Graphics g) {	
+		g.setColor(Color.BLACK);
+		//System.out.println("Se esta pintando");
+		g.drawRect(this.xSuperior, this.ySuperior, this.ancho, this.alto);
+		
+		pintarCirculoCruz(g);
+	}
+	
+	/**
+	 * Comprobar el turno del jugador para pintar una cruz o un círculo
+	 * @param g
+	 */
+	private void pintarCirculoCruz(Graphics g) {
+		//Si el turno es del jugador 1, se pintará un círculo
+		if(this.turnoJugador == Tablero.JUGADOR1){
+			g.setColor(Color.BLUE);
+			g.drawOval(this.xSuperior + 10, this.ySuperior + 10, this.ancho - 20, this.alto - 20);
+		}
+		//Si es del jugador 2, se pintará una cruz	
+		if(this.turnoJugador == Tablero.JUGADOR2) {
+			g.setColor(Color.RED);
+			g.drawLine(this.xSuperior + 10, this.ySuperior + 10, 
+					this.xSuperior + this.ancho - 10, this.ySuperior + this.alto - 10);
+			g.drawLine(this.xSuperior + 10, this.ySuperior + this.alto - 10, 
+					this.xSuperior + this.ancho - 10, this.ySuperior + 10);
+		}
+	}
+		
+	/**
+	 * Detectar en qué posición del canvas se ha hecho click para saber en qué cuadro fue
+	 * @param clickX
+	 * @param clickY
+	 * @return
+	 */
+	public boolean detectarClick(int clickX, int clickY) {
+		
+		if (clickX > this.xSuperior && clickX < (xSuperior + ancho)
+				&& 
+				clickY > this.ySuperior && clickY < (ySuperior + alto)) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Con el método click se decidirá a que jugador le toca colocar círculo o cruz
+	 * @param jugador
+	 */
+	public void click(int jugador) {
+		//En el primer turno hay que comprobar qué turno está seleccionado como primero
+		if (this.turnoJugador == 0) {
+			this.turnoJugador = jugador;
+		}
+		//Si un jugador ha hecho click en una casilla no ocupada, se pasará el turno al siguiente
+		if (Tablero.matrizJugada[x][y] == 0) {
+			Tablero.matrizJugada[x][y] = jugador;
+			tablero.repaint(); //Repintar el tablero para colocar la cruz o el círculo según el turno
+			Tablero.getInstance().revalidate(); //Asegurar que se repinta
+			
+		/*
+		 * Si se ha hecho click en una casilla ocupada se resta un turno, por tanto le tocaría al mismo
+		 * jugador hasta que haga click en un cuadro disponible  
+		 */
+		}else { 
+			turnoJugador --;
+		}
+		
+//		System.out.println("Juego:");
+//		for (int i = 0; i < Tablero.getInstance().getMatrizJugada().length; i++) {
+//			for (int j = 0; j < Tablero.getInstance().getMatrizJugada()[i].length; j++) {
+//				System.out.print(Tablero.getInstance().getMatrizJugada()[i][j] + "\t");
+//			}
+//			System.out.println("");
+//		}
+	}
+	
+	/**
+	 * Determina si existe un ganador en el juego.
+	 * @return Devuelve: 1 si ha ganado el jugador 1; 2 si ha ganado el jugador 2; 
+	 * 0 si no hay ganador y quedan casillas libres; -1 si no hay ganador y no quedan casillas libres
+	 */
+	public int getGanadorDelTablero () {
+		// Busco si existe un ganador mirando las filas
+		for (int i = 0; i < Tablero.matrizJugada.length; i++) {
+			if (Tablero.matrizJugada[i][0] == Tablero.matrizJugada[i][1] && Tablero.matrizJugada[i][1] == Tablero.matrizJugada[i][2]) { 
+				return Tablero.matrizJugada[i][0]; // Si todos los valores de una fila son iguales, 
+										//devuelvo cualquiera de los elementos de esa fila
+			}
+		}
+		// Busco si existe un ganador en las columnas
+		for (int i = 0; i < Tablero.matrizJugada[0].length; i++) {
+			if (Tablero.matrizJugada[0][i] == Tablero.matrizJugada[1][i] && Tablero.matrizJugada[1][i] == Tablero.matrizJugada[2][i]) { 
+				return Tablero.matrizJugada[0][i]; // Si todos los valores de una columna son iguales, 
+										//devuelvo cualquiera de los elementos de esa columna
+			}
+		}
+		// Busco un ganador en la diagonal principal
+		if (Tablero.matrizJugada[0][0] == Tablero.matrizJugada[1][1] && Tablero.matrizJugada[1][1] == Tablero.matrizJugada[2][2]) { 
+			return Tablero.matrizJugada[0][0]; // Devuelvo cualquier elemento de la diagonal principal
+		}
+		// Busco un ganador en la diagonal secundaria
+		if (Tablero.matrizJugada[0][2] == Tablero.matrizJugada[1][1] && Tablero.matrizJugada[1][1] == Tablero.matrizJugada[2][0]) {
+			return Tablero.matrizJugada[0][2]; // Devuelvo cualquier elemento de la diagonal secundaria
+		}
+		
+		/*
+		 *  Si llegó hasta aquí no hay ganador, pero aún quedan dos posibilidades: puede que queden 
+		 *  casillas vacías o puede que no
+		 *  Voy a suponer que no hay casillas vacías y voy a recorrer el tablero buscando alguna casilla vacía
+		 */
+		boolean hayCasillasVacias = false;
+		for (int i = 0; i < Tablero.matrizJugada.length; i++) {
+			for (int j = 0; j < Tablero.matrizJugada[0].length; j++) {
+				if (Tablero.matrizJugada[i][j] == 0) {
+					hayCasillasVacias = true;
+				}
+			}
+		}
+
+		// Devuelvo valores diferentes dependiendo de si existen casillas vacías o no
+		if (hayCasillasVacias == true) {
+			return 0; // Indica que el juego continúa.
+		}
+		else {
+			return -1; // Indica que no quedan casillas vacías, hay empate
+		}
+	}
+
+	/**
+	 * To String utilizado para depurar el código
+	 */
+	@Override
+	public String toString() {
+		return "CuadroDeJuego [xSuperior=" + x + ", ySuperior=" + y + "]";
+	}
+
 	//Getters y setters
 	public int getX() {
 		return x;
@@ -57,4 +210,3 @@ public class CuadroDeJuego {
 	}
 	
 }
-
--- src/juego/Tablero.java
+++ src/juego/Tablero.java
@@ -8,13 +8,23 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 
 public class Tablero extends Canvas {
 
-	private JFrame ventana = null;
-	private List<CuadroDeJuego> cuadro = new ArrayList<CuadroDeJuego>();
+	//Declaración de las distintas variables
+	private List<CuadroDeJuego> cuadro = new ArrayList<CuadroDeJuego>();//Lista de cuadros de tipo CuadroDeJuego
+	public static JFrame ventana = null;//JFrame declarado como null por el momento
+	//Número que referencia a cada jugador
+	public static int JUGADOR1 = 1;
+	public static int JUGADOR2 = 2;
+	private int turnoActual = JUGADOR1; //Turno por el que comienza el juego
+	//Matriz con la que se va comprobando el estado del juego
+	public static int matrizJugada[][] = new int[][] {{0,0,0},
+													  {0,0,0},
+													  {0,0,0}};
 	
 	//Creación de un patrón Singleton
 	private static Tablero instance = null;
@@ -26,8 +36,12 @@ public class Tablero extends Canvas {
 		return instance;
 	}
 	
+	/**
+	 * El juego comienza generando los cuadros de juego
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		Tablero.getInstance().juego();
+		Tablero.getInstance().crearCuadros();
 
 	}
 
@@ -36,70 +50,143 @@ public class Tablero extends Canvas {
 	 * @return 
 	 */
 	public Tablero() {
-		//Crear una nueva ventana con su nombre y dimensiones
-		ventana = new JFrame("Tres en raya");
-		ventana.setBounds(0, 0, 600, 600);
+		ventana = new JFrame("TresEnRaya");
 		
 		// Para colocar objetos sobre la ventana se debe asignar un "layout" (plantilla) al panel principal de la ventana
 		ventana.getContentPane().setLayout(new BorderLayout());
 		//Crear los cuadros en los que se podrá poner una X o un O
-		//cuadro = crearCuadros();
-	
+		ventana.getContentPane().add(this, BorderLayout.CENTER);
+		ventana.setBounds(0, 0, 600, 600);
 		
 		//Detectar cuando se hace clic sobre el tablero 
-	/*	canvas.addMouseMotionListener(new MouseAdapter() {
+		addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseMoved(MouseEvent e) {
+			public void mouseClicked(MouseEvent e) {
 				super.mouseClicked(e);
-				cuadro.
+//				repaint();
+				/*
+				 * Si se hace click con el botón1 del ratón recorrer la lista de cuadros para saber en
+				 * cual de ellos se ha hecho click 
+				 */
+				if(e.getButton() == MouseEvent.BUTTON1) {
+					for(CuadroDeJuego c: cuadro) {
+						if(c.detectarClick(e.getX(), e.getY())) {
+							c.click(turnoActual); //Pasar al método click el jugador que lo ha hecho
+							/*
+							 * Tras cada click hay que comprobar si ha ganado un jugador u otro
+							 * Si del método getGanadorDelTablero devuelve '-1', significa que hay empate
+							 */
+							if (c.getGanadorDelTablero() == -1) {
+								empate();
+							}else { //Si devuelve 1, gana el jugador 1
+								if (c.getGanadorDelTablero() == 1) {
+									ganadorJ1();
+								}else { //Si devuelve 2, gana el jugador 2
+									if (c.getGanadorDelTablero() == 2) {
+										ganadorJ2();
+									}
+								}
+							}
+							//Intercambiar el turno de los jugadores una vez uno haya hecho click
+							if (turnoActual == JUGADOR1) {
+								turnoActual = JUGADOR2;							
+							}else {
+								turnoActual = JUGADOR1;
+							}
+							//System.out.println("Ganador: " + c.getGanadorDelTablero());
+							mostrarMatriz();
+						}
+					}
+				}
 			}			
-		});*/
+		});
 		
-		ventana.getContentPane().add(this, BorderLayout.CENTER);
 		//Evitar que la ventana se repinte con los eventos de Windows
 		ventana.setIgnoreRepaint(true);
 		//Hacer que la ventana sea visible
 		ventana.setVisible(true);
+		//Hacer que la ventana tenga prioridad en el ratón
+		this.requestFocus();
 	}
 	
 	/**
-	 * Método principal del juego
+	 * Método utilizado para crear los cuadros de juego
 	 */
-	public void juego() {
-		
-			this.repaint();
-	}
-	
-	private List<CuadroDeJuego> crearCuadros() {
-		List<CuadroDeJuego> cuadro = new ArrayList<CuadroDeJuego>();
-		
-		/*Calcular cuanto vale un cuadro ya que el tablero es 3x3, al dividir entre 3 el ancho y el alto
-		 * obtenemos cuanto vale 1
-		 */
-	//	int xCuadro = canvas.getWidth()/3;
-		//int yCuadro = canvas.getHeight()/3;
-		
+	private void crearCuadros() {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-		//		CuadroDeJuego c = new CuadroDeJuego(xCuadro*(i+1), yCuadro*(j+1));
-	//			cuadro.add(c);
+				CuadroDeJuego c = new CuadroDeJuego(j, i); //Se generan en las coordenadas deseadas
+				cuadro.add(c); //Añadir cada cuadro a la lista
+				//System.out.println("Se ha creado cuadro");
 			}
 		}
-		
-		// Devuelvo la lista con todos los cuadros del tablero
-		return cuadro;
 	}
 	
 	/**
-	 * Sobrescritura del méotodo paint() ya que el canvas es el tablero 
+	 * Sobrescritura del método paint() ya que el canvas es el tablero 
 	 */
 	@Override
 	public void paint(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(185, 0, 15, 600);
-		g.fillRect(385, 0, 15, 600);
-		g.fillRect(0, 185, 600, 15);
-		g.fillRect(0, 385, 600, 15);
+		super.paint(g);
+		g.setColor(Color.GRAY);//Color del cuadro gris
+		g.fillRect(0, 0, this.getWidth(), this.getHeight()); //Establecer sus dimensiones según el ancho y el alto del canvas
+		//Pintar todos los cuadros guardados en la lista de cuadros
+		for(CuadroDeJuego c: this.cuadro) {
+			c.paint(g);
+		}
+
+	}
+	
+	/**
+	 * Método que muestra en la consola el estado del juego
+	 */
+	public void mostrarMatriz() {
+		System.out.println("\nEstado del juego:");
+		for (int i = 0; i < matrizJugada.length; i++) {
+			for (int j = 0; j < matrizJugada[0].length; j++) {
+				System.out.print(matrizJugada[j][i] + "\t");
+			}
+			System.out.println("");
+		}
+	}
+	
+	/**
+	 * En el caso de que no queden cuadros libres ocurre el empate
+	 */
+	private static void empate() {
+		String [] opciones ={"Aceptar"};
+		int eleccion = JOptionPane.showOptionDialog(ventana,"No quedan casillas libres, el juego ha terminado.","Salir del juego",
+		JOptionPane.YES_NO_OPTION,
+		JOptionPane.QUESTION_MESSAGE, null, opciones, "Aceptar");
+		if (eleccion == JOptionPane.YES_OPTION) {
+			System.exit(0); //Salir de la aplicación
+		}
+	}
+	
+	/**
+	 * En el caso de que gane el jugador 1:
+	 */
+	private static void ganadorJ1() {
+		String [] opciones ={"Aceptar"};
+		int eleccion = JOptionPane.showOptionDialog(ventana,"Ha ganado el jugador 1 (Círculo).","Salir del juego",
+		JOptionPane.YES_NO_OPTION,
+		JOptionPane.QUESTION_MESSAGE, null, opciones, "Aceptar");
+		if (eleccion == JOptionPane.YES_OPTION) {
+			System.exit(0); //Salir de la aplicación
+		}
+	}
+	
+	/**
+	 * En el caso de que gane el jugador 2:
+	 */
+	private static void ganadorJ2() {
+		String [] opciones ={"Aceptar"};
+		int eleccion = JOptionPane.showOptionDialog(ventana,"Ha ganado el jugador 2 (Cruz).","Salir del juego",
+		JOptionPane.YES_NO_OPTION,
+		JOptionPane.QUESTION_MESSAGE, null, opciones, "Aceptar");
+		if (eleccion == JOptionPane.YES_OPTION) {
+			System.exit(0); //Salir de la aplicación
+		}
 	}
 	
 	//Getter y setters
@@ -107,5 +194,9 @@ public class Tablero extends Canvas {
 		return this;
 	}
 
+	public int[][] getMatrizJugada() {
+		return matrizJugada;
+	}
+	
 }
 
